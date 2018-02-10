3b4651f65ebe2e89ac162ae8fe49e0ee6179f928
alonj <alonj@campus.technion.ac.il>
2018-02-10 16:31:19 +0200

    feed now from RSS instead of scraping frontend HTML, enabled multiple sources for feed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import sqlite3
 import scraper
-
-
-def get_html():
-    raw_html = scraper.simple_get('https://www.ynet.co.il/home/0,7340,L-187,00.html')
-    html = scraper.BeautifulSoup(raw_html, 'html.parser')
-    return html.select('.art_headlines_item_content')
+import feedparser
 
 
 def main():
@@ -14,18 +9,24 @@ def main():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM headlines ORDER BY head_ID DESC LIMIT 1")
     result = cursor.fetchone()
-    newsblock = get_html()
+    cursor.execute("SELECT * FROM feeds")
+    sources = cursor.fetchall()
     if result is not None:
         index = int(str(result[0]), 16)
     else:
         index = 0
-    for obj in newsblock:
-        key = hex(index)
-        hl_text = str(obj.select_one('a').get_text())
-        content = str(obj.select_one('.art_headlines_sub_title').get_text())
-        print(key, hl_text)
-        cursor.execute('INSERT INTO headlines(head_ID, headline, subhead) VALUES(?,?,?)', (key, hl_text, content))
-        index += 1
+    for feed in sources:
+        url = feed[1]
+        feedID = feed[0]
+        url_parsed = feedparser.parse(url)
+        for item in url_parsed['items']:
+            key = hex(index)
+            title = item['title']
+            summary = scraper.BeautifulSoup(item['summary'], 'html.parser').get_text()
+            published = item['published']
+            insert_arr = (key, title, summary, published, feedID)
+            cursor.execute('INSERT INTO headlines(head_ID, headline, subhead, timestamp, feedID) VALUES(?,?,?,?,?)', insert_arr)
+            index += 1
     conn.commit()
     conn.close()
     print(index)
